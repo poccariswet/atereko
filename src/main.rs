58a2d7073a972a72091e58e61dd2d64813850a20
poccariswet <s1240231@gmail.com>
2019-08-05 22:21:41 +0900

    [ADD] format command for ffmpeg command

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::process;
 struct Config {
     input_video: String,
     input_music: String,
+    input_time: String,
     output: String,
 }
 
@@ -15,13 +16,24 @@ impl Config {
 
         let input_video = args[1].clone();
         let input_music = args[2].clone();
-        let output = args[3].clone();
+        let input_time = args[3].clone();
+        let output = args[4].clone();
 
         Ok(Config {
             input_video,
             input_music,
+            input_time,
             output,
         })
+    }
+
+    pub fn format_command(&self) -> String {
+        format!("ffmpeg -i {input_v} -itsoffset {input_t} -i {input_m} -c copy -map 0:v:0 -map 1:a:0 {output}",
+                input_v = self.input_video,
+                input_t = self.input_time,
+                input_m = self.input_music,
+                output = self.output
+                )
     }
 }
 
@@ -31,4 +43,7 @@ fn main() {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
+
+    let command = config.format_command();
+    println!("{}", command)
 }
